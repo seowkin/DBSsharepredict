fc0e4766a887cd309db6a1126fbadd5bb22e9d54
seowkin <44171900+seowkin@users.noreply.github.com>
2024-10-19 19:32:31 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/prediction_DBS",methods=["GET","POST"])
+def prediction_DBS():
+    q = float(request.form.get("q"))
+    return(render_template("prediction_DBS.html",r=90.2+(-50.6*q)))
+
+if __name__ == "__main__":
+    app.run()
